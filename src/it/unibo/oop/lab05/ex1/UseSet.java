8e232374c2816590581f4e6c3951b87963cfb92d
Giovanni Antonioni <giovanni.antonioni2@studio.unibo.it>
2020-11-13 17:58:51 +0100

    Complete exercise

--- src/it/unibo/oop/lab05/ex1/UseSet.java
+++ src/it/unibo/oop/lab05/ex1/UseSet.java
@@ -1,6 +1,8 @@
 package it.unibo.oop.lab05.ex1;
 
+import java.util.Iterator;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Example class using {@link Set}.
@@ -34,5 +36,25 @@ public final class UseSet {
          * 
          * 6) Verifies if all the numbers left in the set are even
          */
+    	
+    	Set<String> treeSet = new TreeSet<>();
+    	for (int i = 1; i <= 20; i++) {
+			treeSet.add(String.valueOf(i));
+		}
+    	
+    	System.out.println(treeSet);
+    	
+    	Iterator<String> iterator = treeSet.iterator();
+    	while (iterator.hasNext()) {
+			if(Integer.valueOf(iterator.next()) % 3 == 0) {
+				iterator.remove();
+			}	
+		}
+    	
+    	for (String string : treeSet) {
+			System.out.println(string);
+		}
+    	
+    	
     }
 }
